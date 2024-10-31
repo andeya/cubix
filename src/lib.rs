edae0105729e23a9377055b75581e09f45a0252f
Andeya <andeyalee@outlook.com>
2024-10-31 13:55:50 +0800

    chore: add MaybeString

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Standardized building block components born for Rust craft.
 
-pub use api_response;
+pub use api_response::{self, MaybeString};
 pub use convert_traits::*;
 pub use getset2;
 pub use jwt_claims;
