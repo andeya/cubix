915d0f9bd304a83546675fa3c7a7344a41b18c89
Andeya <andeyalee@outlook.com>
2024-10-31 15:08:10 +0800

    feat: Add ErrWrapper and IntoError

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Standardized building block components born for Rust craft.
 
-pub use api_response::{self, MaybeString};
+pub use api_response::{self, ErrWrapper, IntoError, MaybeString};
 pub use convert_traits::*;
 pub use getset2;
 pub use jwt_claims;
